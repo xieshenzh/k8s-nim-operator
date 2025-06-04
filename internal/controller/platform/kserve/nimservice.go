31a5aa4b4cb1089207350f28f792707abeb03ae2
Xieshen Zhang <xiezhang@redhat.com>
2025-06-04 00:27:04 -0400

    Support KServe platform for NIMService

--- internal/controller/platform/kserve/nimservice.go
+++ internal/controller/platform/kserve/nimservice.go
@@ -19,10 +19,55 @@ package kserve
 import (
 	"context"
 
+	"github.com/go-logr/logr"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	appsv1alpha1 "github.com/NVIDIA/k8s-nim-operator/api/apps/v1alpha1"
+	"github.com/NVIDIA/k8s-nim-operator/internal/conditions"
+	"github.com/NVIDIA/k8s-nim-operator/internal/k8sutil"
+	"github.com/NVIDIA/k8s-nim-operator/internal/render"
+	"github.com/NVIDIA/k8s-nim-operator/internal/shared"
 )
+
+const (
+	// ManifestsDir is the directory to render k8s resource manifests.
+	ManifestsDir = "/manifests"
+)
+
+// NIMServiceReconciler represents the NIMService reconciler instance for KServe platform.
+type NIMServiceReconciler struct {
+	client.Client
+	scheme *runtime.Scheme
+	log    logr.Logger
+
+	updater          conditions.Updater
+	renderer         render.Renderer
+	recorder         record.EventRecorder
+	orchestratorType k8sutil.OrchestratorType
+}
+
+// NewNIMServiceReconciler returns NIMServiceReconciler for KServe platform.
+func NewNIMServiceReconciler(r shared.Reconciler) *NIMServiceReconciler {
+	return &NIMServiceReconciler{
+		Client:   r.GetClient(),
+		scheme:   r.GetScheme(),
+		log:      r.GetLogger(),
+		updater:  r.GetUpdater(),
+		renderer: render.NewRenderer(ManifestsDir),
+		recorder: r.GetEventRecorder(),
+	}
+}
 
 func (r *NIMServiceReconciler) cleanupNIMService(ctx context.Context, nimService *appsv1alpha1.NIMService) error {
 	// TODO: add cleanup logic specific to Kserve modelcache
 	return nil
+}
+
+func (r *NIMServiceReconciler) reconcileNIMService(ctx context.Context, nimService *appsv1alpha1.NIMService) (ctrl.Result, error) {
+	// TODO
+	return ctrl.Result{}, nil
 }
